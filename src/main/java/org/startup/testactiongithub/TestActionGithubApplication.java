3eaf7c3bd1338d6b53f50d3b3d2b1ae3b3732a05
malaz-na <malaznaoura@gmail.com>
2024-05-13 20:41:23 +0300

    init

---
+++ src/main/java/org/startup/testactiongithub/TestActionGithubApplication.java
@@ -0,0 +1,13 @@
+package org.startup.testactiongithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestActionGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestActionGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/startup/testactiongithub/TestActionGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package org.startup.testactiongithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestActionGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
